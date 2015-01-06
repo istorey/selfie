e3118161d7a464e09b882096e14218f46ac7d38b
istorey <wrightstoreys@gmail.com>
2015-01-06 00:05:22 -0500

    last shot

--- app/controllers/selfies_controller.rb
+++ app/controllers/selfies_controller.rb
@@ -1,29 +1,37 @@
 class SelfiesController < ApplicationController
   def index
-      @selfie = Selfie.all
+    @selfies = Selfie.all
   end
 
   def show
-
+    @selfie = Selfie.find(params[:id])
   end
 
   def new
-
+    @selfie = Selfie.new
   end
 
   def create
-
-  end
-
-  def edit
-
-  end
-
-  def update
-
+    @selfie = Selfie.new(selfie_params)
+    if @selfie.save
+      redirect_to '/'
+    else
+      render :new
+    end
   end
 
   def destroy
+    @selfie = Selfie.find(params[:id])
+    if @selfie.destroy
+      redirect_to '/'
+    else
+      redirect_to selfie_path(@selfie)
+    end
+  end
 
+  private
+
+  def selfie_params
+    params.require(:selfie).permit(:body)
   end
 end
